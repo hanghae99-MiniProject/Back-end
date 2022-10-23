9e1cf27636edb1847193074bbc82d0fbe47225e5
bokyungkim <bkyungkeem@daum.net>
2022-10-23 22:27:28 +0900

    - 댓글 작성, 수정, 삭제 기능 구현 - 좋아요 갯수, 댓글 갯수 count 구현

--- src/main/java/com/move/review/controller/CommentController.java
+++ src/main/java/com/move/review/controller/CommentController.java
@@ -2,40 +2,41 @@ package com.move.review.controller;
 
 import com.move.review.controller.response.ResponseDto;
 import com.move.review.controller.request.CommentRequestDto;
+import com.move.review.domain.UserDetailsImpl;
 import com.move.review.service.CommentService;
 import javax.servlet.http.HttpServletRequest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RequiredArgsConstructor
 @RestController
+@RequestMapping(value = "/api")
 public class CommentController {
 
   private final CommentService commentService;
 
-  @PostMapping(value = "/api/auth/comment")
+  // 댓글 작성
+  @PostMapping(value = "/comments")
   public ResponseDto<?> createComment(@RequestBody CommentRequestDto requestDto,
-                                      HttpServletRequest request) {
-    return commentService.createComment(requestDto, request);
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails){
+    return commentService.createComment(requestDto, userDetails);
   }
 
-  @GetMapping(value = "/api/comment/{id}")
-  public ResponseDto<?> getAllComments(@PathVariable Long id) {
-    return commentService.getAllCommentsByPost(id);
+  // 댓글 수정
+  @PutMapping(value = "/comments/{commentId}")
+  public ResponseDto<?> updateComment(@PathVariable Long commentId, @RequestBody CommentRequestDto requestDto,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    return commentService.updateComment(commentId, requestDto, userDetails);
   }
 
-  @PutMapping(value = "/api/auth/comment/{id}")
-  public ResponseDto<?> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto,
-      HttpServletRequest request) {
-    return commentService.updateComment(id, requestDto, request);
-  }
-
-  @DeleteMapping(value = "/api/auth/comment/{id}")
-  public ResponseDto<?> deleteComment(@PathVariable Long id,
-      HttpServletRequest request) {
-    return commentService.deleteComment(id, request);
+  // 댓글 삭제
+  @DeleteMapping(value = "/comment/{commentId}")
+  public ResponseDto<?> deleteComment(@PathVariable Long commentId,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    return commentService.deleteComment(commentId, userDetails);
   }
 }
